0f6912b14e5e01aa67ab3ab640c80f39c4a6b72a
Breno Carneiro <breno.carneiro@gmail.com>
2022-12-09 09:36:18 -0300

    first commit

---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export const ItemContainer = styled.div`
+    width: 80%;
+    font-family: "Segoe UI", "Noto Sans", Helvetica, Arial, sans-serif;
+
+    h3 {
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+    p {
+        font-size: 16px;
+        color: #FAFAFA60;
+        margin-bottom: 20px;
+    }
+    a{
+        color: #FAFAFA95;
+    }
+    a.remover{
+        color: #FF0000;
+    }
+    hr {
+        color: #fafafa60;
+        margin: 20px 0;
+    }
+
+`
---
+++ src/pages/App.js
@@ -0,0 +1,43 @@
+import gitLogo from '../assets/github.png'
+import Input from '../components/Input'
+import Button from '../components/Button'
+import ItemRepo from '../components/ItemRepo'
+import { Container } from './styles'
+import { useState } from 'react'
+import { api } from '../services/api'
+
+function App() {
+  const [currentRepo, setCurrentRepo] = useState('')
+  const [repos, setRepos] = useState([])
+
+  const handleSearchRepo = async () => {
+    const {data} = await api.get(`repos/${currentRepo}`)
+    if(data.id){
+      const exist = repos.find(repo => repo.id === data.id)
+      if(!exist){
+        setRepos(prev => [...prev, data])
+        setCurrentRepo('')
+        return
+      }
+    }
+  }
+
+  const handleRemoveRepo = (id) => {
+    if(id){
+      setRepos([])
+    }
+    console.log(id)
+
+  }
+
+  return (
+    <Container>
+      <img src={gitLogo} width={72} height={72} alt='github logo'/>
+      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)}/>
+      <Button onClick={handleSearchRepo}/>
+      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+    </Container>
+  );
+}
+
+export default App;
